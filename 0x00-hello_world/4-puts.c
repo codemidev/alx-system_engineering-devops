608ab8a320f3977ffff844b4053cfd7eda98e9d2
codemidev <ochaijay81@gmail.com>
2022-07-01 19:46:23 +0000

    Repo update

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - prints with put to the screen
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,12 +1,10 @@
-#include <studio.h>
-
-/** 
- * main - Entry point 
- * Return: Always 0 (Success)
- 
- **/
-
- int main(void) 
-	 { 
-		  return(0); 
-		   }
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * Return:  Always 0 (Success)
+ */
+int main(void)
+{
+return (0); 
+}
