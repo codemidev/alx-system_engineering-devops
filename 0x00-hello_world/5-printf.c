f966fd3da0b35ce420370484a14e54cfd382d791
codemidev <ochaijay81@gmail.com>
2022-07-04 10:17:07 +0000

    Hot fix:5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,10 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a pice of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
